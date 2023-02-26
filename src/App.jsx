c041e51ff8969672825b250030eae03f26f47119
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-26 10:50:16 +0200

    Saved data in store from Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react'
 import getData from './utils/api.js'
-
+import { useSelector, useDispatch } from 'react-redux';
+import { setUrl } from './store/homeSlice.js';
 function App() {
-  
+  const dispatch = useDispatch();
+  const url = useSelector(state => state.home.url);
+
   useEffect(() => {
     apiTest();
   }, [])
 
   const apiTest = async () => {
     const data = await getData("/movie/popular");
+    dispatch(setUrl(data))
   }
 
   return (
     <div className="App">App
+    {url?.total_pages}
     </div>
   )
 }
